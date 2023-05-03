0bf7162f444829209f17169afbe3fcbaf7df37bd
sagarfale <sagar.fale@gmail.com>
2023-05-03 11:38:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+    backend "s3" {
+    bucket = "sfale-bucket"
+    key    = "tfstate/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region     = "us-east-2"
 }
