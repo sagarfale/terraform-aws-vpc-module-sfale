c2f18c7011f9bcf734dd86f1e035aa2140e919bb
sagarfale <sagar.fale@gmail.com>
2023-05-03 08:17:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,39 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-
-  region     = "us-east-2"
+  access_key = "AKIA5JE5MNVPBV3ZGWUM"
+  secret_key = "M20stCzPdLputqub93FrzYfD22xhwAqweCU+0cEU"
+  region     = "us-west-2"
 }
 
-#variable "role_arn" {}
-
-# Create a VPC
-resource "aws_vpc" "example_vpc" {
+# Create the VPC
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "example-vpc"
-  }
 }
 
-# Create an internet gateway
-resource "aws_internet_gateway" "example_igw" {
-  vpc_id = aws_vpc.example_vpc.id
-  tags = {
-    Name = "example-igw"
-  }
+# Create an internet gateway for the VPC
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
-# Create a route table
-resource "aws_route_table" "example_route_table" {
-  vpc_id = aws_vpc.example_vpc.id
-  tags = {
-    Name = "example-route-table"
-  }
+# Create a public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
 }
 
-# Create a route to the internet gateway
-resource "aws_route" "example_internet_route" {
-  route_table_id = aws_route_table.example_route_table.id
+# Create a route table for the public subnet
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Associate the public subnet with the public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create a default route for the public subnet to the internet gateway
+resource "aws_route" "public_subnet_route" {
+  route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.example_igw.id
-}
-
-# Associate the route table with the VPC's main subnet
-resource "aws_route_table_association" "example_association" {
-  subnet_id = aws_vpc.example_vpc.main_route_table_association
-  route_table_id = aws_route_table.example_route_table.id
+  gateway_id             = aws_internet_gateway.my_igw.id
 }
